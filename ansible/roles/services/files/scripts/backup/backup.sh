bec172c8e58ea0ec0b80084751dba692dfacf876
oscar <oscarsjlh@gmail.com>
2025-02-08 17:55:08 +0000

    remove extra folders

--- ansible/roles/services/files/scripts/backup/backup.sh
+++ ansible/roles/services/files/scripts/backup/backup.sh
@@ -21,6 +21,7 @@ echo -e "\e[32mStarting backup script \e[0m"
 echo ""
 echo ""
 restic backup /mnt/main/media/music/ \
+  /home/oscar/jellyfin/config \
   /home/oscar/qbittorrent/configs \
   /mnt/nfs/backup/ \
   --exclude-file /mnt/nfs/backup/.Trash-1000/*/** \
